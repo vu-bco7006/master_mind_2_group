dc9937e2b0f62a24180507113cfe85a6f31a3255
RickTurnerAustralia <frederick.turner@live.vu.edu.au>
2022-06-03 17:56:22 +1000

    Files imported into Python with required fields only - successfully completed

--- print.py
+++ print.py
@@ -1,10 +1,49 @@
+# To open the pc.json file and print to screen
 
-student1_name = "Peter Chen"
-student2_name = "John Smith"
-student3_name = "Mary Roberts"
+import json
 
-team_name = "Awesome!"
+file_pc = open("pc.json", "r")
 
-print("Hello team", team_name)
+try:
+    file_content = file_pc.read()
+    data = json.loads(file_content)
+    print(data)
 
-# print("Your team has members:")
+    print("\n")
+
+finally:
+    file_pc.close()
+
+# To open the csv files and print to screen
+
+import csv
+
+file_orders = open("orders.csv", "r")
+file_order_details = open("order_details.csv", "r")
+
+try:
+    csv_reader_file_orders = csv.reader(file_orders)
+    line_count = 0
+    for row in csv_reader_file_orders:
+        if line_count == 0:
+            print("Column names are: ", row[0], row[1], row[2], row[8])
+        else:
+            print("Order ID:", row[0], "Customer:", row[1], "Freight:", row[2], "Country:", row[8])
+        line_count += 1
+
+    print("\n")
+
+    csv_reader_file_order_details = csv.reader(file_order_details)
+    # line_count = 0
+    for row in csv_reader_file_order_details:
+        if line_count == 0:
+            print("Column names are: ", row[0], row[2], row[3], row[4])
+        else:
+            print("Order ID:", row[0], "Product Category ID:", row[2], "Unit Price:", row[3], "Quantity:", row[4])
+        line_count += 1
+
+finally:
+    file_orders.close()
+    file_order_details.close()
+
+
