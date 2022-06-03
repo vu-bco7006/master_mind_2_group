e3a2405f5bd810a931ee9117a275d7e0d25c9b46
RickTurnerAustralia <frederick.turner@live.vu.edu.au>
2022-06-03 23:41:19 +1000

    Files imported into Python file using pandas & numpy; question 1 completed; question 2 in progress (difficulties obtaining a rank order from highest to lowest country).

--- print.py
+++ print.py
@@ -1,3 +1,4 @@
+"""
 # To open the pc.json file and print to screen
 
 import json
@@ -6,14 +7,15 @@ file_pc = open("pc.json", "r")
 
 try:
     file_content = file_pc.read()
-    data = json.loads(file_content)
-    print(data)
+    data_file_pc = json.loads(file_content)
+    print(data_file_pc)
 
     print("\n")
 
 finally:
-    file_pc.close()
+    file_pc.close()"""
 
+"""
 # To open the csv files and print to screen
 
 import csv
@@ -45,5 +47,51 @@ try:
 finally:
     file_orders.close()
     file_order_details.close()
+"""
 
+# To import using pandas and numpy
+
+import pandas as pd
+import numpy as np
+
+frame_pc = pd.read_json("pc.json")
+print(f'This is file pc.json : \n \n {frame_pc} \n')
+
+frame_orders = pd.read_csv("orders.csv")
+print(f'This is file orders.csv : \n \n {frame_orders} \n')
+
+frame_order_details = pd.read_csv("order_details.csv")
+print(f'This is file order_details.csv : \n \n {frame_order_details} \n')
+
+# Questions 1 - To calculate the freight costs in July 2021
+
+freight_costs = frame_orders['freight']
+
+print(f'This is the column of freight costs: \n \n {freight_costs} \n')
+
+print(f'The freight costs in July 2021 were ($): {freight_costs.sum()} \n')
+
+# Question 2 - To find the countries with the most orders
+
+# country = frame_orders['ship_country']
+
+# print(f'This is the country using header function with max 60: \n \n{country_sort.head(60)}')
+# print(f'This is the country using describe function: \n \n{country_sort.describe}')
+
+# print(f'This is the column of countries: \n \n{country} \n')
+
+# print(f'The sort by country is: \n \n{country_sort} \n')
+
+for name, group in frame_orders.groupby('ship_country'):  # perhaps need to use if/then function to restrict to n>=20 rows (eg Germany & USA)
+    print(name)
+    print(group)
+
+for name in frame_orders['ship_country']:
+    print(name)
+
+
+
+
+# doesn't work as string variable:  print(f'The count of country for shipping: {country.sum()} \n')
+# print(country.groupby(['ship_country']))
 
