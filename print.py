7e28fc0cc5f7637deb7f7a723a322c518e9cc57a
RickTurnerAustralia <frederick.turner@live.vu.edu.au>
2022-06-05 21:30:17 +1000

    Question 4 in progress - stuck on last step #5.

--- print.py
+++ print.py
@@ -1,67 +1,18 @@
-"""
-# To open the pc.json file and print to screen
-
-import json
-
-file_pc = open("pc.json", "r")
-
-try:
-    file_content = file_pc.read()
-    data_file_pc = json.loads(file_content)
-    print(data_file_pc)
-
-    print("\n")
-
-finally:
-    file_pc.close()"""
-
-"""
-# To open the csv files and print to screen
-
-import csv
-
-file_orders = open("orders.csv", "r")
-file_order_details = open("order_details.csv", "r")
-
-try:
-    csv_reader_file_orders = csv.reader(file_orders)
-    line_count = 0
-    for row in csv_reader_file_orders:
-        if line_count == 0:
-            print("Column names are: ", row[0], row[1], row[2], row[8])
-        else:
-            print("Order ID:", row[0], "Customer:", row[1], "Freight:", row[2], "Country:", row[8])
-        line_count += 1
-
-    print("\n")
-
-    csv_reader_file_order_details = csv.reader(file_order_details)
-    # line_count = 0
-    for row in csv_reader_file_order_details:
-        if line_count == 0:
-            print("Column names are: ", row[0], row[2], row[3], row[4])
-        else:
-            print("Order ID:", row[0], "Product Category ID:", row[2], "Unit Price:", row[3], "Quantity:", row[4])
-        line_count += 1
-
-finally:
-    file_orders.close()
-    file_order_details.close()
-"""
-
-# To import using pandas and numpy
+# Importing pandas and numpy into Python:
 
 import pandas as pd
 import numpy as np
 
+# Importing files:
+
 frame_pc = pd.read_json("pc.json")
-print(f'This is file pc.json : \n \n {frame_pc} \n')
+# print(f'This is file pc.json : \n \n {frame_pc} \n')
 
 frame_orders = pd.read_csv("orders.csv")
-print(f'This is file orders.csv : \n \n {frame_orders} \n')
+# print(f'This is file orders.csv : \n \n {frame_orders} \n')
 
 frame_order_details = pd.read_csv("order_details.csv")
-print(f'This is file order_details.csv : \n \n {frame_order_details} \n')
+# print(f'This is file order_details.csv : \n \n {frame_order_details} \n')
 
 # Questions 1 - To calculate the freight costs in July 2021
 
@@ -76,7 +27,7 @@ print(f'Question 1 - The freight costs in July 2021 is ($): {freight_costs.sum()
 country_count = frame_orders['ship_country'].value_counts()
 # print(country_count)
 
-print(f'Question 2 - The 5 countries with the highest number of orders in July 2021: \n{country_count}\n')
+print(f'Question 2 - The 2 countries with the highest number of orders in July 2021: \n{country_count.head(2)}\n')
 
 """ 
 # Alternative options for obtaining the solution (2 examples):
@@ -104,8 +55,10 @@ Steps - In data file 'order_details.csv':
         total_cost = unit_price x quantity,
         total_cost will be a new variable (column).
 2.  Use the Pandas 'groupby' function to obtain product_category_id revenue for July 2021.
-3.  Obtain the highest revenue amount using NumPy's 'max' function, then select it's product category by filtering in pandas.
-4.  Select the "category_name" in 'pc.json' for the product category with the highest revenue amount. 
+3.  Adding a key with values (as a list) to the list of dictionaries in 'frame_pc.json'.
+4.  Obtain the highest revenue amount using NumPy's 'max' function.
+5.  Select the item with maximum value from within the list of dictionaries.
+6.  Print as output the product category name with the maximum revenue.
 """
 
 # Step 1:
@@ -113,28 +66,46 @@ Steps - In data file 'order_details.csv':
 a = frame_order_details['unit_price']
 b = frame_order_details['quantity']
 frame_order_details['total_cost'] = a * b  # 'total_cost' is a new variables in frame_order_details
-
-# print(a, b)
-
 # print(frame_order_details['total_cost'])
 
-# Step 2:
+# Step 2 - Obtaining sum of total cost for orders by 'category_id':
 
-product_category_id_revenue = frame_order_details[['product_category_id', 'total_cost']].groupby(['product_category_id']).sum()
-print(product_category_id_revenue)
+product_category_rev = frame_order_details[['product_category_id', 'total_cost']].groupby(['product_category_id']).sum()
+# print(product_category_rev)
 
-# Step 3:
+# Step 3 - Adding a key and values (as a list) to list of dictionaries in 'frame_pc.json':
 
-highest_revenue_amount = product_category_id_revenue['total_cost'].max()  # 'highest_revenue_amount' is a new variable
-x = product_category_id_revenue[product_category_id_revenue['total_cost'] == highest_revenue_amount]  # filtering
-print(f'\n{x}\n')
+product_category_revenue = list(product_category_rev['total_cost'])  # need to check if this should be a tuple or list
+# print(product_category_revenue)
 
-# step 4:
+product_category_revenue = [int(q) for q in product_category_revenue]  # convert to a list of integers
+# print(product_category_revenue)
 
-# print(product_category_id_revenue['product_category_id'])
+frame_pc['product_revenue'] = product_category_revenue
+# print(f'This is the list of dictionaries with "product_revenue" column: \n{frame_pc}')
 
-for j, k in frame_pc.items():  # refer to lecture 7 and workshop codes in Python for selecting 'product_name' with 'category_id' == x['product_category_id']
-    print(j, k)
+frame_pc["category_id"] = [int(s) for s in frame_pc["category_id"]]  # delete this line if not required
+frame_pc["category_name"] = list(frame_pc["category_name"])  # delete this line if not required
 
-# y = frame_pc.get("category_id", "1")
-# print(y)
+
+# Step 4 - Obtaining the product category with the highest revenue in July 2021:
+
+highest_revenue_amount = product_category_rev['total_cost'].max()  # 'highest_revenue_amount' is a new variable
+x = product_category_rev[product_category_rev['total_cost'] == highest_revenue_amount]  # filtering
+# print(f'\n{x}\n')
+
+p = ()
+
+for j, k in x.items():
+    p = int(k)  # variable 'k' is the revenue amount (integer) for the product category that has the highest revenue
+
+# print(p)
+
+# step 5 - Select the item with maximum value from within the list of dictionaries 'product_category_revenue':
+
+for i in frame_pc.items():
+    if i["total_cost"] == 'p':
+        frame_pc = i
+        print(i)
+        print(f"{frame_pc['category_name']} contributed to the highest revenue of {frame_pc['total_cost']}.")
+
