17050c844ac958d6f587f8fdfecc3e6ed64fc7dd
RickTurnerAustralia <frederick.turner@live.vu.edu.au>
2022-06-10 13:49:43 +1000

    Question 4 - code finalised.

--- print.py
+++ print.py
@@ -17,40 +17,28 @@ frame_order_details = pd.read_csv("order_details.csv")
 # Questions 1 - To calculate the freight costs in July 2021
 
 freight_costs = frame_orders['freight']
-
 # print(f'This is the column of freight costs: \n \n {freight_costs} \n')
 
-print(f'Question 1 - The freight costs in July 2021 is ($): {freight_costs.sum()} \n')
+print(f'Question 1 -\nThe total freight costs in July 2021 is ($){freight_costs.sum()} \n')
 
 # Question 2 - To find the countries with the most orders
 
 country_count = frame_orders['ship_country'].value_counts()
 # print(country_count)
 
-print(f'Question 2 - The 2 countries with the highest number of orders in July 2021: \n{country_count.head(2)}\n')
-
-""" 
-# Alternative options for obtaining the solution (2 examples):
-
-for name, group in frame_orders.groupby('ship_country'):
-    print(name)
-    print(group)
-
-for name in frame_orders['ship_country']:
-    print(name)
-"""
+print(f'Question 2 -\nThe 2 countries with the highest number of orders in July 2021:\n{country_count.head(2)}\n')
 
 # Question 3 - To find the 3 customers with the highest number of orders
 
 customer_name_count = frame_orders['customer_name'].value_counts()
-# print(f'The number of orders per customer in July 2021 is: \n{customer_name_count}\n')
+# print(f'The number of orders per customer in July 2021 is: \n{customer_name_count.head(20)}\n')
 
-print(f'Question 3 - The 3 customers with the highest number of orders in July 2021: \n{customer_name_count.head(3)}\n')
+print(f'Question 3 -\nThe 3 customers with the highest number of orders in July 2021:\n{customer_name_count.head(3)}\n')
 
 # Question 4 - To find the product category with the highest sales revenue
 
 """
-Steps - In data file 'order_details.csv':
+Strategy for obtaining answer to Q4: Steps - In data file 'order_details.csv' -
 1.  The total cost per order is calculated as -
         total_cost = unit_price x quantity,
         total_cost will be a new variable (column).
@@ -78,46 +66,23 @@ product_category_rev = frame_order_details[['product_category_id', 'total_cost']
 product_category_revenue = list(product_category_rev['total_cost'])  # need to check if this should be a tuple or list
 # print(product_category_revenue)
 
-product_category_revenue = [int(q) for q in product_category_revenue]  # convert to a list of integers
-# print(product_category_revenue)
-
 frame_pc['product_revenue'] = product_category_revenue
 # print(f'This is the list of dictionaries with "product_revenue" column: \n{frame_pc}')
-
-frame_pc["category_id"] = [int(s) for s in frame_pc["category_id"]]  # delete this line if not required
-frame_pc["category_name"] = list(frame_pc["category_name"])  # delete this line if not required
-
 
 # Step 4 - Obtaining the product category with the highest revenue in July 2021:
 
 highest_revenue_amount = product_category_rev['total_cost'].max()  # 'highest_revenue_amount' is a new variable
-x = product_category_rev[product_category_rev['total_cost'] == highest_revenue_amount]  # filtering
-# print(f'\n{x}\n')
 
-p = ()
+# step 5 - Select the item with maximum value from within the list of dictionaries 'product_revenue':
 
-for j, k in x.items():
-    p = int(k)  # variable 'k' is the revenue amount (integer) for the product category that has the highest revenue
+y = frame_pc[frame_pc['product_revenue'] == highest_revenue_amount]
 
-# print(p)
+z1 = y.get("category_name")
+z2 = y.get("description")
+z3 = y.get("product_revenue")
 
-# step 5 - Select the item with maximum value from within the list of dictionaries 'product_category_revenue':
-
-"""
-for i in frame_pc:
-    if i["product_revenue"] == p:
-        frame_pc = i
-        print(i)
-        print(f"{frame_pc['category_name']} contributed to the highest revenue of {frame_pc['product_revenue']}.")
-"""
-
-z = {'product_revenue': [p]}
-q = frame_pc.isin(z)
-print(f'Questions 4 - The product category with the highest revenue: \n \n{q}\n')
-
-r = frame_pc[frame_pc.isin([p])]
-print(r)
-
-
-g = frame_pc['category_name']
-print(f'\n{g}\n')
+print(f"Question 4 -\nThe product category with the highest sales revenue is: \n\n"
+      f"{y}\n\n"
+      f"In summary, the category's name is:\n{z1}\n\n"
+      f"This category's total revenue for this month is ${highest_revenue_amount}\n\n"
+      f"A brief description of this category is:\n{z2}")
